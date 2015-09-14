1b766b579897969151a3b2b6682a391506871da5
David Op de Beeck <davidopdebeeck@hotmail.com>
2015-09-14 17:09:17 +0200

    Added indenting

--- web.py
+++ web.py
@@ -11,22 +11,22 @@ app = Flask(__name__)
 
 @app.route('/almas', methods=['GET'])
 def web_get_all_almas():
-    return json.dumps(utils.get_all_almas())
+    return json.dumps(utils.get_all_almas(), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>', methods=['GET'])
 def web_get_alma(alma_id):
-    return json.dumps(utils.get_alma(alma_id))
+    return json.dumps(utils.get_alma(alma_id), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>/menu/<int:week_id>', methods=['GET'])
 def web_get_alma_menu_from_week(alma_id, week_id):
-    return json.dumps(utils.get_menu(alma_id, date.today().year, week_id))
+    return json.dumps(utils.get_menu(alma_id, date.today().year, week_id), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>/menu/<int:week_id>/<int:year_id>', methods=['GET'])
 def web_get_alma_menu_from_week_and_year(alma_id, week_id, year_id):
-    return json.dumps(utils.get_menu(alma_id, year_id, week_id))
+    return json.dumps(utils.get_menu(alma_id, year_id, week_id), indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
