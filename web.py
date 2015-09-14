d3e0b11ff713e88a10e672a526622880d5459a79
David Op de Beeck <davidopdebeeck@hotmail.com>
2015-09-14 16:34:59 +0200

    Added new endpoints /almas/[:id]>/menu/[:week-id] and  /almas/[:id]>/menu/[:week-id]/[:year-id]

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask
+from datetime import date
 import utils
 import json
 
@@ -18,9 +19,14 @@ def web_get_alma(alma_id):
     return json.dumps(utils.get_alma(alma_id))
 
 
-@app.route('/almas/<int:alma_id>/menu', methods=['GET'])
-def web_get_alma_menu(alma_id):
-    return json.dumps(utils.get_menu(alma_id))
+@app.route('/almas/<int:alma_id>/menu/<int:week_id>', methods=['GET'])
+def web_get_alma_menu_from_week(alma_id, week_id):
+    return json.dumps(utils.get_menu(alma_id, date.today().year, week_id))
+
+
+@app.route('/almas/<int:alma_id>/menu/<int:week_id>/<int:year_id>', methods=['GET'])
+def web_get_alma_menu_from_week_and_year(alma_id, week_id, year_id):
+    return json.dumps(utils.get_menu(alma_id, year_id, week_id))
 
 
 if __name__ == '__main__':
