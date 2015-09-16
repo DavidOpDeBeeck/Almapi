6c858ff03ccdf2bb379361aab9a50dc2d6575e13
David Op de Beeck <davidopdebeeck@hotmail.com>
2015-09-16 16:15:57 +0200

    Reduced json import

--- web.py
+++ web.py
@@ -1,33 +1,31 @@
 #!flask/bin/python
 from datetime import date
 from flask import Flask
-import json
+from json import dumps
 
 from utils import utils
 
 app = Flask(__name__)
 
 
-# WEB FUNCTIONS
-
 @app.route('/almas', methods=['GET'])
 def web_get_all_almas():
-    return json.dumps(utils.get_all_almas(), indent=4, sort_keys=True)
+    return dumps(utils.get_all_almas(), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>', methods=['GET'])
 def web_get_alma(alma_id):
-    return json.dumps(utils.get_alma(alma_id), indent=4, sort_keys=True)
+    return dumps(utils.get_alma(alma_id), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>/menu/<int:week_id>', methods=['GET'])
 def web_get_alma_menu_from_week(alma_id, week_id):
-    return json.dumps(utils.get_menu(alma_id, date.today().year, week_id), indent=4, sort_keys=True)
+    return dumps(utils.get_menu(alma_id, date.today().year, week_id), indent=4, sort_keys=True)
 
 
 @app.route('/almas/<int:alma_id>/menu/<int:week_id>/<int:year_id>', methods=['GET'])
 def web_get_alma_menu_from_week_and_year(alma_id, week_id, year_id):
-    return json.dumps(utils.get_menu(alma_id, year_id, week_id), indent=4, sort_keys=True)
+    return dumps(utils.get_menu(alma_id, year_id, week_id), indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
